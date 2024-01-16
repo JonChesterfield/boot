5539c5283b3e7d5db7e03c2fb969697c1cd338d9
Jon Chesterfield <jonathanchesterfield@gmail.com>
2024-01-16 21:07:34 +0000

    drop the ssh.config part

--- boot.sh
+++ boot.sh
@@ -20,20 +20,6 @@ else
     ssh-keygen -f ~/.ssh/github_id_rsa -t rsa -b 4096 -C "github-`hostname`" -N ""
 fi
 
-cat << EOF > ~/.ssh/config
-Include ~/.ssh/config.local
-
-Compression yes
-IdentitiesOnly yes
-ForwardAgent no
-ServerAliveInterval 60
-ServerAliveCountMax 10
-
-EOF
-
-touch ~/.ssh/authorized_keys
-chmod 644 ~/.ssh/authorized_keys
-
 # Otherwise git ignores the github key
 # This will be somewhat annoying when cloning from another server
 git config --global core.sshCommand 'ssh -i ~/.ssh/github_id_rsa'
